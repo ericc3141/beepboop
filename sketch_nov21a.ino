c1f5f18d15604345e1522b7dadb93079d7f6f68d
Crystal <liang.cjs@gmail.com>
2019-11-22 17:47:48 -0800

    added servo code

--- sketch_nov21a.ino
+++ sketch_nov21a.ino
@@ -1,33 +1,80 @@
-  int enable_a = 8;
-  int enable_b = 9;
-  int in1 = 3;
-  int in2 = 4;
-  int in3 = 5;
-  int in4 = 6;
+#include "SR04.h"
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+int turned = 0;
+
+long a;
+int enable_a = 7;
+int in1 = 3;
+int in2 = 4;
+int in3 = 5;
+int in4 = 6;
+
+SR04 sr04_left = SR04(13,12);
+SR04 sr04_right = SR04(11,10);
 
 void setup() {
+  Serial.begin(9600);
+  delay(1000);
 
   pinMode(enable_a, OUTPUT);
-//  pinMode(enable_b, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
 
+  
 }
 
 void loop()
 {
 
-  // goes forward
-  // when I_In 3 = LOW, and I_IN 4 = HIGH, goes backwards
-  digitalWrite(in4, LOW);
-  digitalWrite(in3, HIGH);
-//  analogWrite(enable_b, 80);
+ long left = sr04_left.Distance();
+ long right = sr04_right.Distance();
 
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  analogWrite(enable_a, 80);
+  if (left > 6 || right > 6) {
+    digitalWrite(in4, HIGH);
+    digitalWrite(in3, LOW);
+    
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    analogWrite(enable_a, 0);
+
+    
+    
+    
+    // turn servo
+    if (turned == 0) {
+      myservo.attach(2);
+      myservo.write(180);
+      turned = 1;
+    } else {
+      myservo.detach();
+    }
+  
+  
+  } else {
+    digitalWrite(in4, HIGH);
+    digitalWrite(in3, LOW);
+    
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    analogWrite(enable_a, 100);
+  }
+
+  Serial.print("left");
+  Serial.print(left);
+  Serial.println("cm");
+  
+  Serial.print("right");
+  Serial.print(right);
+  Serial.println("cm");
+
+
 }
 
 // goforward
