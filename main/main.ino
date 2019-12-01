300130e979c12b1e828a8c511d2f44019968fd16
Eric <ericc3141@users.noreply.github.com>
2019-11-30 18:36:11 -0800

    basic obstacle detect

--- main/main.ino
+++ main/main.ino
@@ -31,6 +31,9 @@ typedef struct {
   struct {
     ir_t left, right;
   } line;
+  struct {
+    ir_t left, mid, right;
+  } obstacle;
   joystick_t joystick;
 } sensors_t;
 sensors_t sense = {};
@@ -47,6 +50,15 @@ bool lineFollowed = false;
 state_t state;
 int start_button = 10;
 int tic_tac_state = 0;
+
+typedef enum {D_STOP, D_FORWARD, D_REVERSE, D_TURN} drive_t;
+drive_t drive_state = D_STOP;
+int drive_remaining;
+
+typedef struct {
+  unsigned long now, prev, dt;
+} clock_t;
+clock_t gtime = {0, 0, 1};
 
 
 ros::NodeHandle nh;
@@ -75,7 +87,9 @@ void setup() {
 
   sense.ultra.left = ultra_setup(34, 38);
   sense.ultra.right = ultra_setup(32, 36);
-  sense.line.left = ir_setup(A0);
+  sense.obstacle.left = ir_setup(A0);
+  sense.obstacle.right = ir_setup(A1);
+  sense.obstacle.mid = ir_setup(A2);
   sense.joystick = joystick_setup(A0, A1, A2, 512, 512);
 
   act.motor.right = motor_setup(8, 3, 4);
@@ -95,6 +109,28 @@ void drive(int power, int turn) {
   Serial.print(right);
   motor_drive(act.motor.left, left);
   motor_drive(act.motor.right, right);
+}
+
+void drivefor() {
+  drive_remaining -= gtime.dt;
+  if (drive_state == D_FORWARD) {
+    drive(50, 0);
+  } else if (drive_state == D_REVERSE) {
+    drive(-50, 0);
+    if (drive_remaining < 0) {
+      drive_state = D_TURN;
+      drive_remaining = 500;
+    }
+  } else if (drive_state == D_TURN) {
+    drive(0, 92);
+    if (drive_remaining < 0) {
+      drive_state = D_FORWARD;
+    }
+  } else {
+    drive(0, 0);
+  }
+
+  //drive((int)(255.*sense.joystick.y), (int)(255.*sense.joystick.x));
 }
 
 void displayState(leds_t leds, state_t state) {
@@ -122,8 +158,10 @@ bool edgeDetected(sensors_t sense) {
 }
 
 bool obstacleDetected(sensors_t sense){
-  // TODO
-  return false;
+  const float thresh = 300;
+  return sense.obstacle.left.light < thresh
+      || sense.obstacle.right.light < thresh
+      || sense.obstacle.mid.light < thresh;
 }
 
 bool spotDetected(sensors_t sense){
@@ -137,18 +175,27 @@ bool inclineDetected(sensors_t sense){
 
 void loop() {
   delay(10);
+  gtime.prev = gtime.now;
+  gtime.now = millis();
+  gtime.dt = gtime.now - gtime.prev;
   Serial.print("\n");
   //nh.spinOnce();
   ultra_read(sense.ultra.left);
   ultra_read(sense.ultra.right);
   ir_read(sense.line.left);
+  ir_read(sense.line.right);
+  ir_read(sense.obstacle.left);
+  ir_read(sense.obstacle.right);
+  ir_read(sense.obstacle.mid);
   joystick_read(sense.joystick);
   if (digitalRead(buttons.estop) == LOW) {
     state = S_ESTOP;
   }
 
   displayState(leds, state);
+  drivefor();
   if (state == S_ESTOP) {
+    drive_state = D_STOP;
     return;
   }
 
@@ -156,9 +203,8 @@ void loop() {
 //  Serial.print(sense.ultra.left.dist);
 //  Serial.print("\t");
 //  Serial.print(sense.ultra.right.dist);
-  Serial.print(sense.line.left.light);
+  Serial.print(sense.obstacle.left.light);
   tictac_loop(act.tictac);
-  //drive((int)(255.*sense.joystick.y), (int)(255.*sense.joystick.x));
 
 
   if (state == S_ESTOP){ return; }
@@ -166,25 +212,21 @@ void loop() {
     if (digitalRead(buttons.start) == LOW){
       delay(3000);
       state = S_RUN;
+      drive_state = D_FORWARD;
     } else{
       delay(100);
       return;
     }
   } else if (state == S_RUN){
     if (edgeDetected(sense)|| obstacleDetected(sense)){
-//      tictac_drop(act.tictac);
-//      if (act.tictac.done) {
-//        state = S_FINISH;
-//      }
-//      drive(0, 0);
+        drive_state = D_REVERSE;
+        drive_remaining = 1000;
     } else if (spotDetected(sense) && lineFollowed){
-      drive(0, 0);
       state = S_FINISH;
+      drive_state = D_STOP;
     } else{
       if (sense.line.left.light < 290) {
-        drive(60, -30);
       } else {
-        drive(60, 30);
       }
     }
 
